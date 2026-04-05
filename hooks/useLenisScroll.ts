4f2c44d76f9753be21f4a09649617638fcc6b02f
chriscs0625 <IT23254398@MY.SLIIT.LK>
2026-04-05 20:41:24 +0530

    fix(hooks): remove invalid direction option from LenisOptions

--- hooks/useLenisScroll.ts
+++ hooks/useLenisScroll.ts
@@ -11,12 +11,6 @@ export function useLenisScroll() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
       infinite: false,
     })
 
